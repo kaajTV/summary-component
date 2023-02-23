3819a660c6bb5502fc0cf3b274942035cfc34e52
Anton kaaj Holmgren <anton.holmgren3@hotmail.com>
2023-02-23 15:50:16 +0100

    First Commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,90 +1,91 @@
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
+import reactionSvg from '../../assets/images/icon-reaction.svg'
+import memorySvg from '../../assets/images/icon-memory.svg'
+import verbalSvg from '../../assets/images/icon-verbal.svg'
+import visualSvg from '../../assets/images/icon-visual.svg'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className={styles.content}>
+        <div className={styles.result__content}>
+          <p className={styles.result__title}>Your Result</p>
+          <div className={styles.result__score}>
+            <span>76</span>
+            <p>of 100</p>
+          </div>
+          <h2 className={styles.result__great}>Great</h2>
+          <p className={styles.result__text}>You scored higher than <span>65%</span> of the people who have taken these tests.</p>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+        <div className={styles.summary}>
+          <h3 className={styles.summary__title}>Summary</h3>
+          <ul className={styles.summary__list}>
+            <li className={styles.summary__items}>
+              <div className={styles.summary__items_container}>
+                <div className={styles.summary__items_container_svg}>
+                  <span className={styles.summary__items_svg}>
+                    <Image src={reactionSvg} alt="reaction" />
+                  </span>
+                  <p className={styles.summary__items_reaction}>Reaction</p>
+                </div>
+                <div className={styles.summary__items_rating}>
+                  <span className={styles.summary__items_rating_score}>80</span>
+                  <p className={styles.summary__items_rating_total}>/ 100</p>
+                </div>
+              </div>
+            </li>
+            <li className={styles.summary__items}>
+              <div className={styles.summary__items_container}>
+                <div className={styles.summary__items_container_svg}>
+                  <span className={styles.summary__items_svg}>
+                    <Image src={memorySvg} alt="memory" />
+                  </span>
+                  <p className={styles.summary__items_memory}>Memory</p>
+                </div>
+                <div className={styles.summary__items_rating}>
+                  <span className={styles.summary__items_rating_score}>92</span>
+                  <p className={styles.summary__items_rating_total}>/ 100</p>
+                </div>
+              </div>
+            </li>
+            <li className={styles.summary__items}>
+              <div className={styles.summary__items_container}>
+                <div className={styles.summary__items_container_svg}>
+                  <span className={styles.summary__items_svg}>
+                    <Image src={verbalSvg} alt="verbal" />
+                  </span>
+                  <p className={styles.summary__items_verbal}>Verbal</p>
+                </div>
+                <div className={styles.summary__items_rating}>
+                  <span className={styles.summary__items_rating_score}>61</span>
+                  <p className={styles.summary__items_rating_total}>/ 100</p>
+                </div>
+              </div>
+            </li>
+            <li className={styles.summary__items}>
+              <div className={styles.summary__items_container}>
+                <div className={styles.summary__items_container_svg}>
+                  <span className={styles.summary__items_svg}>
+                    <Image src={visualSvg} alt="visual" />
+                  </span>
+                  <p className={styles.summary__items_visual}>Visual</p>
+                </div>
+                <div className={styles.summary__items_rating}>
+                  <span className={styles.summary__items_rating_score}>72</span>
+                  <p className={styles.summary__items_rating_total}>/ 100</p>
+                </div>
+              </div>
+            </li>
+          </ul>
+          <div className={styles.button__container}>
+            <button className={styles.button}>Continue</button>
+          </div>
         </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   )
